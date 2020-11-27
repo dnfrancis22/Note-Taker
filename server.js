90ef50b1bd025985890cd6b1c36daa611b2c3d2f
Dennis Francis <dnfrancis22@gmail.com>
2020-11-26 22:28:02 -0500

    added html routes

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/api/config",(req, res) => {
-    res.json({success: true,});
-});
+// app.get("/api/config",(req, res) => {
+//     res.json({success: true,});
+// });
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
 app.listen(PORT, function() {
     console.log(`Server is running on http://localhost:${PORT}`);

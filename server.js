18a73439a958f07958fbc0838972bd1abe824376
Dennis Francis <dnfrancis22@gmail.com>
2020-12-01 13:11:55 -0500

    added delete route.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var path = require("path");
 var fs = require("fs");
 var db = require("./db/db.json");
 //uuid
-const { v4: uuidv4 } = require('uuid');
-console.log(uuidv4())
+const { v4: uuidv4 } = require("uuid");
+console.log(uuidv4());
 
 var app = express();
 
@@ -19,46 +19,54 @@ app.get("/notes", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-
-
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
-      
     if (err) {
-      console.error(err)
+      console.error(err);
     }
     return res.json(JSON.parse(data));
 
-    console.log(data)
+    console.log(data);
   });
 });
 app.post("/api/notes", (req, res) => {
-    //data passed in in an obj(bodyparser=>req.body) that contains title and text
-    //once I get my data what do we do
-    //save it to json file db (array of objects).
-    //push it (obj) into the array
-    //update the obj with an id
-    req.body.id = uuidv4();
-    db.push(req.body)
-    console.log(db)
-    fs.writeFile("./db/db.json", JSON.stringify(db), (err) =>
-  err ? console.error(err) : console.log('Success!')
-  
-);
-
-//for delete you will need to look through your array for a match, once you get it 
+  //data passed in in an obj(bodyparser=>req.body) that contains title and text
+  //once I get my data what do we do
+  //save it to json file db (array of objects).
+  //push it (obj) into the array
+  //update the obj with an id
+  req.body.id = uuidv4();
+  db.push(req.body);
+  console.log(db);
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+});
+//for delete you will need to look through your array for a match, once you get it
 //get the index location to remove the item (splice)
 //req.params
+app.delete("/api/notes/:id", (req, res) => {
+  const deleteNote = req.params.id;
 
-
-    
-  });
+  fs.readFile("./db/db.json", JSON.stringify(db), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+  db = JSON.parse(data);
+  for (var i = 0; i < db.length; i++) {
+    if (db[i].id === Number(deleteNote)) {
+      db.splice([i], 1);
+    }
+  }
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+});
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 app.listen(PORT, function () {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
